d14e5c42e2be80cb1b6fee7734ddf2ea44605113
Eric <eric@simontonsoftware.com>
2017-07-27 07:37:18 -0400

    remove `isMatch`
    - I mistook how it should work & only used it for `isEqual`

--- lib/src/lang/is-equal.ts
+++ lib/src/lang/is-equal.ts
@@ -1,12 +1,10 @@
-import {isMatch} from './is-match';
-
 /**
  * Performs a deep comparison between two values to determine if they are equivalent.
  *
  * Differences from lodash:
  * - cannot handle circular references
  * - summary: only meant to work with primitives, arrays and plain objects. For the details, read on.
- * - only considers own enumerable properties (and not own enumerable property symbols). NB this means it does not support (among others):
+ * - only considers own enumerable properties (and not own enumerable property symbols). NB this means it does not support (among other things):
  *   - primitive objects (e.g. `Object(false)`)
  *   - `arguments` objects
  *   - Date objects
@@ -26,5 +24,10 @@ export function isEqual(value: any, other: any) {
       return false;
     }
   }
-  return isMatch(value, other);
+  for (const key of Object.getOwnPropertyNames(other)) {
+    if (!isEqual(value[key], other[key])) {
+      return false;
+    }
+  }
+  return true;
 }
