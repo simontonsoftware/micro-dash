4dee67b66a9de1abc0bb5c1303234369875aaf05
Eric <eric@simontonsoftware.com>
2018-08-03 21:16:55 -0400

    e2e test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { kebabCase } from 'micro-dash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'micro-dash-platform';
+  title = kebabCase('micro dash');
 }
