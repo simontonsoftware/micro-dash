d0347a2362f060e806cfbe66c0855cd8a357895a
Eric <eric@simontonsoftware.com>
2018-08-11 12:58:51 -0400

    chore: release script

--- standard-version-postbump.ts
+++ standard-version-postbump.ts
@@ -2,9 +2,9 @@ import { writeFileSync } from 'fs';
 import { format } from 'prettier';
 
 const packageJson = require('./package.json');
-const libPackageJson = require('./projects/ng-app-state/package.json');
+const libPackageJson = require('./projects/micro-dash/package.json');
 writeFileSync(
-  './projects/ng-app-state/package.json',
+  './projects/micro-dash/package.json',
   format(JSON.stringify({ ...libPackageJson, version: packageJson.version }), {
     parser: 'json',
   }),
